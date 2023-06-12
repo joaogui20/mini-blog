fae47198315b3f7c77c3c9c9fc57722abbc55c46
João Guilherme <azazazazaisjoao@gmail.com>
2023-06-12 12:42:38 -0400

    Login de usuário

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -75,6 +75,44 @@ export const useAuthentication = () => {
         signOut(auth)
     }
 
+    // Login
+    const login = async(data) => {
+
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+            
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false)
+
+        } catch (error) {
+            
+            let systemErrorMessage
+
+            if (error.message.includes("user-not-found")) {
+
+                systemErrorMessage = "Usuário não encontrado."
+
+            } else if (error.message.includes("wrong-password")) {
+
+                systemErrorMessage = "Senha incorreta."
+
+            } else {
+
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
+
+            }
+
+            setError(systemErrorMessage)
+            setLoading(false)
+
+        }
+
+    }
+
     useEffect(() => {
         return () => setCancelled(true)
     }, [])
@@ -85,5 +123,6 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login,
     }
 }
