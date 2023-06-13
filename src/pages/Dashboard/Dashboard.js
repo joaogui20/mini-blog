99d4391fe07e7045cb9fbcb2a4f4ee75e2159e17
João Guilherme <azazazazaisjoao@gmail.com>
2023-06-13 15:56:03 -0400

    Excluindo post

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 // Hooks
 import { useAuthValue } from "../../context/AuthContext"
 import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+import { useDeleteDocument } from "../../hooks/useDeleteDocument"
 
 const Dashboard = () => {
   const { user } = useAuthValue()
@@ -12,9 +13,7 @@ const Dashboard = () => {
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
 
-  const deleteDocument = (id) => {
-
-  }
+  const { deleteDocument } = useDeleteDocument("posts")
 
   if (loading) {
     return <p>Carregando...</p>
