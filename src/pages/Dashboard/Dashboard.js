c25c19ecc55ddb545f71d03e639bcdbd0d195a50
João Guilherme <azazazazaisjoao@gmail.com>
2023-06-13 15:45:20 -0400

    CSS da dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import { useFetchDocuments } from "../../hooks/useFetchDocuments"
 
 const Dashboard = () => {
   const { user } = useAuthValue()
-  const uid = user.id
+  const uid = user.uid
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
 
@@ -21,37 +21,45 @@ const Dashboard = () => {
   }
 
   return (
-    <div className={styles.noposts}>
-        <h2>Dashboard</h2>
-        <p>Gerencie os seus posts</p>
-        {posts && posts.length === 0 ? (
-          <div>
-            <p>Não foram encontrados posts</p>
-            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
-          </div>
-        ) : (
-          <div>
-            <>
-              <div>
-                <span>Título</span>
-                <span>Ações</span>
-              </div>
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.post_header}>
+          <span>Título</span>
+          <span>Ações</span>
+        </div>
+      )}
 
-              {posts && posts.map((post) =>
-                <div key={post.id}>
-                  <p>{post.title}</p>
-                  <div>
-                    <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver postagem</Link>
-                    <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar postagem</Link>
-                    <button onChange={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
-                  </div>
-                </div>
-              )}
-            </>
+      {posts &&
+        posts.map((post) => (
+          <div className={styles.post_row} key={post.id}>
+            <p>{post.title}</p>
+            <div className={styles.actions}>
+              <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                Ver
+              </Link>
+              <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                Editar
+              </Link>
+              <button
+                onClick={() => deleteDocument(post.id)}
+                className="btn btn-outline btn-danger"
+              >
+                Excluir
+              </button>
+            </div>
           </div>
-        )}
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
